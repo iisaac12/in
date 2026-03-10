6c69bdaba0d0673630faff184396a5ed0f011901
apiszs <hafizhalkautsar@student.ub.ac.id>
2026-03-10 17:17:16 +0700

    tugas orang 2 (2), nambah api.php

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,4 @@ use App\Http\Controllers\InventarisController;
 
 Route::get('/inventaris', [InventarisController::class, 'index']);
 Route::get('/inventaris/{id}', [InventarisController::class, 'show']);
-Route::post('/inventaris', [InventarisController::class, 'store']);
+Route::post('/inventaris', [InventarisController::class, 'store']); 
